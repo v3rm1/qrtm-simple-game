ef0e83bcfbf2a72cf039617abe939e081569a784
v3rm1 <varunravivarma@gmail.com>
2021-06-07 14:13:27 +0200

    Updated experiments.

--- cartpole-PER/discretizer.py
+++ cartpole-PER/discretizer.py
@@ -42,6 +42,16 @@ class CustomDiscretizer:
                 binary_rep[n_places-i+1] = 1
         return binary_rep
 
+    def _less_than_binned_binarizer(self, fp_num, range_min, range_max, n_bins=16):
+        binary_rep = np.zeros(shape=n_bins, dtype=int)
+        bin_delta = (np.absolute(range_max) + np.absolute(range_min))/n_bins
+        for i in range(0, n_bins):
+            bin_max = range_min + i * bin_delta
+            if fp_num <= bin_max:
+                binary_rep[i] = 1
+        return binary_rep
+
+
     def _simple_binarizer(self, fp_num, bits):
         if fp_num < 0:
             return np.fromiter(np.binary_repr(3, width=bits+1), int)
@@ -92,6 +102,15 @@ class CustomDiscretizer:
             op_2 = self._unsigned_binarizer(input_state[1], -500, 500, n_bins+1)
             op_3 = self._unsigned_binarizer(input_state[2], -42, 42, n_bins+1)
             op_4 = self._unsigned_binarizer(input_state[3], -500, 500, n_bins+1)
+
+        elif bin_type == "L":
+            # lesser than binned binarizer
+            # based on arXiv:1905.04199v2 [cs.LG]
+            op_1 = self._less_than_binned_binarizer(input_state[0], -1.5, 1.5, n_bins+1)
+            op_2 = self._less_than_binned_binarizer(input_state[1], -100, 100, n_bins+1)
+            op_3 = self._less_than_binned_binarizer(input_state[2], -15, 15, n_bins+1)
+            op_4 = self._less_than_binned_binarizer(input_state[3], -100, 100, n_bins+1)
+
         else:
             op_1 = self._simple_binarizer(input_state[0], bits=n_bins)
             op_2 = self._simple_binarizer(input_state[1], bits=n_bins)
