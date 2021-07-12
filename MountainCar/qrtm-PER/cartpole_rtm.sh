52b1fac290917db690af6e955d9091c79d434005
v3rm1 <varunravivarma@gmail.com>
2021-07-12 17:01:22 +0200

    Initial commit for MountainCar environment.

---
+++ MountainCar/qrtm-PER/cartpole_rtm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --time=20:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=PER_qrtm_cartpole
+#SBATCH --mem=2000
+module restore thesis_mods
+python3 ~/../../data/s3893030/cartpole-PER/run_simulations.py
