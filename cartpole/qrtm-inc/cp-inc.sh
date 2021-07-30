4d8976cc6e104ddccf71fe480329c3dce2fdfb91
V.R.Varma@student.rug.nl <s3893030@peregrine.hpc.rug.nl>
2021-07-30 12:16:03 +0200

    Removed incremental directory.

--- cartpole/qrtm-inc/cp-inc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --time=20:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --job-name=PER_qrtm_cartpole
-#SBATCH --mem=2000
-module restore thesis_mods
-python3 ~/../../data/s3893030/qrtm-simple-game/cartpole/qrtm-inc/cp-inc.sh
